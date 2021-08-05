91cc881e188548e11fd7f8846265a54f4c92221e
sabalazs <sabalazs@gmail.com>
2021-08-05 11:36:52 +0200

    fixing naming typo

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -2,4 +2,4 @@ export * from './authService';
 export * from './ordersService';
 export * from './productsService';
 export * from './usersService';
-export * from './imageService';
+export * from './renaming';

03e69b126d55f0098015208b6c36427ba03f09cb
sabalazs <sabalazs@gmail.com>
2021-08-20 15:16:24 +0200

    tests: new test for ProductCard

---
+++ frontend/src/components/place-order/ProductCard.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProductCard from './ProductCard';
+import ContextWrapper from '../../testUtil';
+
+describe('Product Card', () => {
+  it('renders correctly', () => {
+    const { queryByText } = render(
+      <ContextWrapper>
+        <ProductCard product={{
+            isAvailable: true,
+            labels: ['új termék'],
+            _id: '6118c257d371de51c8bc7c41',
+            name: 'Vajas croissant',
+            price: 1000,
+        }} />
+      </ContextWrapper>,
+    );
+    expect(queryByText('Vajas croissant')).toBeTruthy();
+    expect(queryByText('új termék')).toBeTruthy();
+    expect(queryByText('1000,- Ft')).toBeTruthy();
+  });
+});

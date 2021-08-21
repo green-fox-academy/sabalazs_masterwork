8675c56e6b748b1bc55bdf0be0d0ed3dc41f3664
sabalazs <sabalazs@gmail.com>
2021-08-21 09:24:24 +0200

    test: new test - ProductForm

---
+++ frontend/src/components/admin/products/ProductForm.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProductForm from './ProductForm';
+import ContextWrapper from '../../../testUtil';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Admin new product form', () => {
+  it('renders correctly', () => {
+    const { queryByText, queryByPlaceholderText } = render(
+      <ContextWrapper>
+        <MemoryRouter>
+          <ProductForm />
+        </MemoryRouter>
+      </ContextWrapper>
+    );
+    expect(queryByText('Név:')).toBeTruthy();
+    expect(queryByPlaceholderText('Termék neve')).toBeTruthy();
+    expect(queryByText('Ár:')).toBeTruthy();
+    expect(queryByPlaceholderText('Termék ára')).toBeTruthy();
+    expect(queryByText('Kép:')).toBeTruthy();
+    expect(queryByText('Rendelhető')).toBeTruthy();
+    expect(queryByText('Címkék:')).toBeTruthy();
+    expect(queryByText('Mentés')).toBeTruthy();
+    expect(queryByText('Vissza')).toBeTruthy();
+  });
+});
